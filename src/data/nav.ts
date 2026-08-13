80d1e70e4b8ce9dca856af47648e169fbabccb79
Antoni Kaleta <antonikaleta@gmail.com>
2026-08-13 17:36:21 -0400

    feat: update logo size, modify navigation links, and enhance homepage design
    - Reduced logo height from 100px to 90px for better alignment.
    - Commented out the Blog link in the navigation for future consideration.
    - Added a Background component to the BaseLayout for a dynamic background effect.
    - Enhanced the homepage header with a new styling for the title, including a glowing effect.
    - Removed unnecessary elements from the homepage and added CSS for improved visual appeal.
    - Introduced a new Background component with a shooting star animation for a more engaging user experience.

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -8,7 +8,7 @@ export const NAV_LINKS: NavLink[] = [
   { href: "/", label: "Home", key: "" },
   { href: "/properties", label: "Our Listings", key: "properties" },
   { href: "/about", label: "About", key: "about" },
-  { href: "/blog", label: "Blog", key: "blog" },
+  //{ href: "/blog", label: "Blog", key: "blog" },
   { href: "/fairHousing", label: "Fair Housing", key: "fairHousing" },
   { href: "/contact", label: "Contact", key: "contact" },
 ];
