96f69b6e4754f695ecc9b5a2fb0c4cd02e7e2f66
Antoni Kaleta <antonikaleta@gmail.com>
2026-08-31 17:05:59 -0400

    feat: enhance navigation with dropdown support and update styling; add Fair Housing and SOP pages

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -4,11 +4,31 @@ export interface NavLink {
   key: string;
 }
 
-export const NAV_LINKS: NavLink[] = [
+export interface Dropdown {
+  label: string;
+  key: string;
+  links: NavLink[];
+}
+
+export type NavItem = NavLink | Dropdown;
+
+export const NAV_LINKS: NavItem[] = [
   { href: "/", label: "Home", key: "" },
   { href: "/properties", label: "Our Listings", key: "properties" },
   { href: "/about", label: "About", key: "about" },
   //{ href: "/blog", label: "Blog", key: "blog" },
-  { href: "/fairHousing", label: "Fair Housing", key: "fairHousing" },
+  { label: "Policies", key: "policies", 
+    links: [
+      {
+        href: "/policies/fairHousing",
+        label: "Fair Housing",
+        key: "fairhousing"
+      },
+      {
+        href: "/policies/sop",
+        label: "Standard Operating Procedure",
+        key: "sop"
+      }
+    ] },
   { href: "/contact", label: "Contact", key: "contact" },
 ];
